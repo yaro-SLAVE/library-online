5c3854736bd1ef90dfc891c0f80a54b24acd95d0
valen <valentin22812@gmail.com>
2025-05-26 02:35:02 +0800

    improve some styles in  print modal

--- client/src/modules/staff/components/PrintModal.vue
+++ client/src/modules/staff/components/PrintModal.vue
@@ -95,7 +95,9 @@ const open = defineModel<boolean>();
   color: var(--color-primary-600);
 }
 
-.qr-container {
+.qr-container { 
+  margin: auto;
+  height: fit-content; 
   padding: 1rem;
   background: white;
   border-radius: 12px;
