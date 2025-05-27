b5a1e205a03bc298fdc989073951258327a8ebee
yaro-SLAVE <85624526+yaro-SLAVE@users.noreply.github.com>
2025-05-27 10:44:20 +0800

    add tests for order_staff

---
+++ backend/library_service/tests/test_staff_order.py
@@ -0,0 +1,80 @@
+from django.test import Client
+import pytest
+
+from rest_framework.test import APIClient
+
+from django.test import TestCase
+from library_service.models.order import Order, OrderHistory, OrderItem, Library
+from library_service.tests.test_auth import authorize
+
+from library_service.tests.opac_mock import BookId
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class ModelsTest(TestCase):
+    def test_order_creation(self):
+        user = User.objects.get(pk = 1)
+        library = Library.objects.get(pk = 1)
+        order = Order.objects.create(user = user, library = library)
+        order_item = OrderItem.objects.create(order=order, book_id=BookId.ISTU_AAAA_XXXX.value)
+        order_history = OrderHistory.objects.create(order=order, status=OrderHistory.Status.NEW)
+
+
+        self.assertEqual(order.user.email, user.email)
+        self.assertEqual(order.library.address, library.address)
+        self.assertEqual(order_item.order, order)
+        self.assertEqual(order_history.order, order)
+        self.assertEqual(order_history.status, OrderHistory.Status.NEW)
+
+class APITest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_get_orders(self):
+        user = User.objects.get(pk = 1)
+        library = Library.objects.get(pk = 1)
+        order = Order.objects.create(user = user, library = library)
+        order_item = OrderItem.objects.create(order=order, book_id=BookId.ISTU_AAAA_XXXX.value)
+        order_history = OrderHistory.objects.create(order=order, status=OrderHistory.Status.NEW)
+
+        response = self.client.get('/api/staff/order/?status=new')
+        data = response.json()
+
+        assert user.first_name == data[0]['reader']['first_name']
+        assert order.pk == data[0]['order_id']
+
+    def test_get_order_by_id(self):
+        user = User.objects.get(pk = 1)
+        library = Library.objects.get(pk = 1)
+        order = Order.objects.create(user = user, library = library)
+        order_item = OrderItem.objects.create(order=order, book_id=BookId.ISTU_AAAA_XXXX.value)
+        order_history = OrderHistory.objects.create(order=order, status=OrderHistory.Status.NEW)
+
+        response = self.client.get('/api/staff/order/1/')
+        data = response.json()
+
+        assert user.first_name == data[0]['reader']['first_name']
+        assert order.pk == data[0]['order_id']
+
+    def test_put_status(self):
+        user = User.objects.get(pk = 1)
+        library = Library.objects.get(pk = 1)
+        order = Order.objects.create(user = user, library = library)
+        order_item = OrderItem.objects.create(order=order, book_id=BookId.ISTU_AAAA_XXXX.value)
+        order_history = OrderHistory.objects.create(order=order, status=OrderHistory.Status.NEW)
+
+        authorize(self.client)
+
+        response = self.client.put('/api/staff/order/1/', {
+            "status": {
+                "status": "processing",
+                "description": "описание"
+            },
+            "books": []
+        })
+        
+        assert response.status_code == 200
+
+        
